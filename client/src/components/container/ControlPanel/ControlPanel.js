f00fcefd24150e300ab77647370dbad686919dc4
Naetirat Songsomboon <naetirat.s@gmail.com>
2019-06-08 17:23:32 +0700

    Update ControlPanel component

--- client/src/components/container/ControlPanel/ControlPanel.js
+++ client/src/components/container/ControlPanel/ControlPanel.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './ControlPanel.css';
 
 const ControlPanel = props => {
-  return <div>ControlPanel</div>;
+  return (
+    <main>
+      <h1>Control Panel</h1>
+      <div>Under Construction...</div>
+    </main>
+  );
 };
 
 export default ControlPanel;
