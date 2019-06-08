a9e7ee271ba9c690ea2cd339c02df0dba2c00459
Naetirat Songsomboon <naetirat.s@gmail.com>
2019-06-08 17:15:49 +0700

    Implement React-router

--- client/src/components/Layout/Main/Main.js
+++ client/src/components/Layout/Main/Main.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Profile from '../../container/Profile/Profile';
+import ControlPanel from '../../container/ControlPanel/ControlPanel';
 
 class Main extends React.Component {
   render() {
-    return <Profile />;
+    return (
+      <BrowserRouter>
+        <Route path="/" exact component={Profile} />
+        <Route path="/config" exact component={ControlPanel} />
+      </BrowserRouter>
+    );
   }
 }
 
